eab27cfbba06db52a62cc3a9397d7b381fde1f11
Mayar Arabi <mayararaby246@gmail.com>
2020-12-07 07:33:57 -0800

    split the code into two files config and index

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "TODO_LIST_DB";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+?>
