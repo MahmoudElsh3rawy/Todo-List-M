a60e0a13fc3a2149feacd03a6e3ace43b3902b23
AhmedTurkiii <ahmedturki2232014@gmail.com>
2020-12-07 15:26:06 +0200

    ready to go

--- todolist.php
+++ todolist.php
@@ -1,79 +1,72 @@
-
 <?php
-    // connect mysql
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "todo_list";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    };
-    // create database
-    $sql = "CREATE DATABASE todo_list";
-    $createDB = $conn->query($sql);
-    if($createDB){
-       echo "  database created  ";
-    }else{
-         echo $conn->error;
-         }
-
-    // use database 
-
-     $usedb = "USE todo_list;";
-     $runusedb = $conn->query($usedb);
-     if($runusedb){
-         echo "  used  ";
-     }else{
-    echo $conn->error;
-     };
-          // create table and insert values 
-     $table="CREATE TABLE `users_table`(
-        `user_id` int(11) NOT NULL auto_increment,
-        `name` varchar(100) NOT NULL,
-        `password` varchar(100)  NOT NULL,  
-         PRIMARY KEY  (`user_id`)
-      );";
-                
-     $runtable = $conn->query($table);
-     if($runtable){
-         echo "created table";
-     }else{
-         echo $conn->error;
-     }
-
-     $insertvalues = "INSERT INTO users_table(name, password ) values ('turki', '123456');";
-  $runsql = $conn->query($insertvalues);
-  if ($runsql){
-      echo "inserted";
-  }else{
-      $conn->error; };
-
-      
-     $table2="CREATE TABLE `tasks_table`(
-        `task_id` int(11) NOT NULL auto_increment,
-        `task` text (100) NOT NULL,
-         PRIMARY KEY  (`task_id`),
-         FOREIGN KEY (`user_id`)
-      );";
-  $runtable = $conn->query($table2);
-  if($runtable){
-      echo "created table";
-  }else{
-      echo $conn->error;
-  }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "TODO_LIST_DB";
 
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE if not exists TODO_LIST_DB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+$usedb = "USE TODO_LIST_DB;";
+$runusedb = $conn->query($usedb);
+if($runusedb){
+    echo "  used  ";
+}else{
+echo $conn->error;
+};
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS users_table (
+id INT(6) AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table users_table created successfully <br>";
+} else {
+  echo "Error creating table  1  : " . mysqli_error($conn);
+}
 
 
-  $insertvalues = "INSERT INTO users_table(name, password ) values ('turki', '123456');";
-  $runsql = $conn->query($insertvalues);
-  if ($runsql){
-      echo "inserted";
-  }else{
-      $conn->error; };
-
-
+$sql = "CREATE TABLE IF NOT EXISTS tasks_table (
+    id INT(6)  AUTO_INCREMENT PRIMARY KEY,
+    task text(30) NOT NULL,
+    userid int not null,
+    FOREIGN KEY (userid) REFERENCES users_table(id)
+	
+    )";
     
+    if (mysqli_query($conn, $sql)) {
+      echo "Table tasks_table created successfully";
+    } else {
+      echo "Error creating table: " . mysqli_error($conn);
+    }
+
+$sql = "INSERT INTO users_table (username) VALUES ('John')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "user created successfully";
+} else {
+  echo "Error  record 1 : " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$sql = "INSERT INTO tasks_table (task, userid) VALUES ('sonthing good!', 1)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "task created successfully";
+} else {
+  echo "Error record 2 : " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
