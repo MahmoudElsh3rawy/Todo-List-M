007fbca2c01fec7c63a56aaca09030d14578aad5
Mayar Arabi <mayararaby246@gmail.com>
2020-12-08 04:17:43 -0800

    Delete Done and word

--- Delete.php
+++ Delete.php
@@ -1,7 +1,7 @@
 <?php
-include_once("config.php");
-include_once("CreatDBandTabels.php");
-$id= $_GET['id'];
-$result=mysqli_query($conn,"DELETE FROM  tasks_table WHERE id=$id");
-header("Location:index.php");
+    include_once("config.php");
+    $id= $_GET['id'];
+    $sql= mysqli_query($conn,"ALTER TABLE tasks_table AUTO_INCREMENT =1");
+    $result=mysqli_query($conn,"DELETE FROM  tasks_table WHERE id=$id");
+    header("Location:index.php");
 ?>
