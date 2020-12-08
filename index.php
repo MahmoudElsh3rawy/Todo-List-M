d652620d2e88d0ab753c51795b991eb573583c6a
Mayar Arabi <mayararaby246@gmail.com>
2020-12-08 04:20:46 -0800

    make index more organized

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       }
     }
   </script> -->
-<form action="index.php" method="post" name="form1">
+<form action="Add.php" method="post" name="form1">
 		<table width="25%">
 			<tr>
 				<td>Text</td>
@@ -36,38 +36,22 @@
 			</tr>
 		</table>
 </form>
-<?php
-    include_once("config.php");
-    include_once("CreatDBandTabels.php");
-    if(isset($_POST['Submit'])) {
-        if (isset($_POST['task']))
-        {
-            $task = $_POST['task']; 
-            $result = mysqli_query($conn,"INSERT INTO tasks_table (`task`,`userid`) VALUES ('$task',1)");
-            echo "<label style='color: green;'>User added successfully.</label>";                
-        }
-    }
-    $sqlSelect = "SELECT * FROM tasks_table ORDER BY id ";
-    $result = mysqli_query($conn, $sqlSelect) or die( mysqli_error($conn));
-?>
-    <table width='80%' border=1>
-        <tr>
-            <th>id</th> 
-            <th>Tasks</th>
+    <table width='80%'>
+            <th align='left'>Tasks</th>
             <?php
-               while($tasks = mysqli_fetch_array($result)) {
+                include_once("config.php");
+                $sqlSelect = "SELECT * FROM tasks_table";
+                $result = mysqli_query($conn, $sqlSelect) or die( mysqli_error($conn));
+                while($tasks = mysqli_fetch_array($result)) {
                 echo "<tr>";
-                echo "<td>".$tasks['id']."</td>";
                 echo "<td>".$tasks['task']."</td>";
-                
                 echo "<td><a href='Delete.php?id=$tasks[id]' style='color:red'>Delete </a>";
                 echo" <a href='Edit.php?id=$tasks[id]'style='color:green'>Edit </a>
                  </td>";
                }
-            exit;
+            //    $ToDoList = null;
+            //    $task = NULL;
             ?>
-
-            
         </tr>
 
     </table>
