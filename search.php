e88a160074ab1e0475adf02f0cc80ca23b600e36
Mayar Arabi <mayararaby246@gmail.com>
2020-12-07 17:01:13 -0800

    Ajex update

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 include_once("config.php");
 include_once("CreatDBandTabels.php");
 $task= $_POST['task'];
-$sql = "SELECT task from tasks_table where name LIKE '$task%'"; 
+$sql = "SELECT task from tasks_table where task LIKE '$task%'"; 
     $result  = mysqli_query($conn,$sql);
 	while($row=mysqli_fetch_array($result))
     {
