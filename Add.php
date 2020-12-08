ef2a4608b71fcb8514a6b8ffbdd5c97fc48db8d7
Mayar Arabi <mayararaby246@gmail.com>
2020-12-08 04:19:40 -0800

    fix the bug and add work

--- Add.php
+++ Add.php
@@ -1,15 +1,12 @@
 <?php
-if(isset($_POST['Submit'])) {
-    if (isset($_POST['task']))
-        {
-        $task = $_POST['task']; 
-        include_once("config.php");
-        include_once("CreatDBandTabels.php");
-        $id= $_GET['id'];
-        $result = mysqli_query($conn,"INSERT INTO tasks_table (`task`,`userid`) VALUES ('$task',1)");
-        // echo "<label style='color: green;'>User added successfully.</label>";
-        // }
-        header("Location:index.php");
-    }
-?>
+    include_once("config.php");
 
+        if (isset($_POST['Submit'])&& isset($_POST['task']) && ! empty($_POST['task']))
+        {
+            $task = $_POST['task']; 
+            $result = mysqli_query($conn,"INSERT INTO tasks_table (`task`,`userid`) VALUES ('$task',1)");
+            echo "<label style='color: green;'>User added successfully.</label>";                
+        }
+    header("Location:index.php");
+    
+?>
